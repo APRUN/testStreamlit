b1db412cd26478d9ce83a1202173bd12e3885390
Khalil Ahmad <iamkhalil2005@gmail.com>
2025-02-21 20:47:50 +0500

    added file

---
+++ file.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+
+
+st.toast("Warming up...")
+st.error("Error message")
+st.warning("Warning message")
+st.info("Info message")
+st.success("Success message")
